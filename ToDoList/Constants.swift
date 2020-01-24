4d82ef5c23f4544dc60cfb339eb595599e6b465e
Martin Kleppmann <martin@kleppmann.com>
2020-01-24 11:21:34 +0000

    Use my own Realm cloud instance

--- ToDoList/Constants.swift
+++ ToDoList/Constants.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 struct Constants {
-    static let MY_INSTANCE_ADDRESS = "muse-trial.us1a.cloud.realm.io"
+    static let MY_INSTANCE_ADDRESS = "martinkl.us1a.cloud.realm.io"
     static let AUTH_URL  = URL(string: "https://\(MY_INSTANCE_ADDRESS)")!
-    static let REALM_URL = URL(string: "realms://\(MY_INSTANCE_ADDRESS)/ToDo")!
+    static let REALM_URL = URL(string: "realms://\(MY_INSTANCE_ADDRESS)/~/ToDo")!
 }
