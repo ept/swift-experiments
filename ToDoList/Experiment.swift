68154c72e512c7876bc88d6a44f95a795b1cb9bb
Martin Kleppmann <martin@kleppmann.com>
2020-01-24 16:23:29 +0000

    Observe all properties of an object

--- ToDoList/Experiment.swift
+++ ToDoList/Experiment.swift
@@ -4,7 +4,7 @@ class ToDoItem: NSObject {
     @objc dynamic var itemId: String = UUID().uuidString
     @objc dynamic var body: String = ""
     @objc dynamic var isDone: Bool = false
-    var timestamp: Date = Date()
+    @objc dynamic var timestamp: Date = Date()
 
     deinit {
         print("freeing ToDoItem")
@@ -15,34 +15,47 @@ class ToDoItem: NSObject {
     }
 }
 
-class MyObserver : NSObject {
-    let target: ToDoItem
-    var observation: NSKeyValueObservation?
+struct ClassInfo {
+    let className: String
+    let properties: [String : [String : String]]
+}
 
-    init(observe target: ToDoItem) {
+class MyObserver : NSObject {
+    let classInfo: ClassInfo
+    let target: ToDoItem
+    private var context = 0
+
+    init(classInfo: ClassInfo, observe target: ToDoItem) {
+        self.classInfo = classInfo
         self.target = target
         super.init()
-        observation = target.observe(\.isDone, options: [.old, .new]) {
-            _, change in
-            print("isDone changed from \(change.oldValue!) to \(change.newValue!)")
+        for property in classInfo.properties.keys {
+            target.addObserver(self, forKeyPath: property, options: [.old, .new], context: &context)
         }
     }
 
     deinit {
+        for property in classInfo.properties.keys {
+            target.removeObserver(self, forKeyPath: property, context: &context)
+        }
         print("freeing MyObserver")
+    }
+
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        // This pattern is from https://stackoverflow.com/a/25219216
+        guard context == &self.context else {
+            super.observeValue(forKeyPath: keyPath, of: object, change: change, context: context)
+            return
+        }
+
+        guard let keyPath = keyPath else { return }
+        guard let change = change else { return }
+        print("\(keyPath) changed from \(change[.oldKey]!) to \(change[.newKey]!)")
     }
 }
 
 struct Experiment {
-    static func str(_ cString: UnsafePointer<CChar>?) -> String? {
-        if let cString = cString {
-            return String(validatingUTF8: cString)
-        } else {
-            return nil
-        }
-    }
-
-    static func model<Element: NSObject>(_ type: Element.Type) {
+    static func model<Element: NSObject>(_ type: Element.Type) -> ClassInfo {
         // Mirror API lets us get stored properties of an instance (but not a class?)
         // https://www.swiftbysundell.com/articles/reflection-in-swift/
         // Example of using the Mirror API for JSON encoding:
@@ -54,38 +67,36 @@ struct Experiment {
             }
         }
 
-        print("type: \(String(cString: class_getName(type)))")
         var propCount: UInt32 = 0
+        var properties: [String : [String : String]] = [:]
         if let props = class_copyPropertyList(type, &propCount) {
             for i in 0..<Int(propCount) {
-                let prop = props[i]
-                let name = String(cString: property_getName(prop))
-                let attrString = str(property_getAttributes(prop)) ?? ""
-                print("property \(name): attr \(attrString)")
-
-                var attrCount: UInt32 = 0
-                if let attrs = property_copyAttributeList(prop, &attrCount) {
+                var attrCount: UInt32 = 0, attrDict: [String : String] = [:]
+                if let attrs = property_copyAttributeList(props[i], &attrCount) {
                     for j in 0..<Int(attrCount) {
                         let attrName = String(cString: attrs[j].name)
                         let attrValue = String(cString: attrs[j].value)
-                        print("    attribute \(j): \(attrName) = \(attrValue)")
+                        attrDict[attrName] = attrValue
                     }
                     free(attrs)
                 }
+
+                let propName = String(cString: property_getName(props[i]))
+                properties[propName] = attrDict
             }
             free(props)
         }
-        // other functions in runtime.h: object_getIvar, object_setIvar,
-        // class_copyProtocolList, class_conformsToProtocol, class_getSuperclass,
-        // class_copyMethodList, class_getInstanceMethod,
-        // class_copyPropertyList, class_getProperty
+
+        let className = String(cString: class_getName(type))
+        return ClassInfo(className: className, properties: properties)
     }
 
     static func run() {
-        model(ToDoItem.self)
-
+        let info = model(ToDoItem.self)
         let instance = ToDoItem()
-        let observer = MyObserver(observe: instance)
+        let observer = MyObserver(classInfo: info, observe: instance)
         instance.isDone = true
+        instance.body = "hello"
+        instance.timestamp = Date()
     }
 }
